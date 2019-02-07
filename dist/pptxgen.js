d99b4b79daec69fc49850c6f75189f7c16e60547
Rokas <r.diedonis@yahoo.com>
2019-02-07 20:09:11 +0200

    added tick mark changes for charts

--- dist/pptxgen.js
+++ dist/pptxgen.js
@@ -3723,8 +3723,8 @@ var PptxGenJS = function(){
 			strXml += '  <c:tickLblPos val="nextTo"/>';
 		}
 		else {
-			strXml += '  <c:majorTickMark val="out"/>';
-			strXml += '  <c:minorTickMark val="none"/>';
+			strXml += ' <c:majorTickMark val="' + (opts.valAxisMajorUnitType ? opts.valAxisMajorUnitType : 'out') + '"/>';
+			strXml += ' <c:minorTickMark val="' + (opts.valAxisMinorUnitType ? opts.valAxisMinorUnitType : 'none') + '"/>';
 			strXml += '  <c:tickLblPos val="'+ (opts.catAxisLabelPos || opts.barDir == 'col' ? 'low' : 'nextTo') +'"/>';
 		}
 		strXml += '  <c:spPr>';
@@ -3816,8 +3816,8 @@ var PptxGenJS = function(){
 			strXml += '  <c:tickLblPos val="nextTo"/>';
 		}
 		else {
-			strXml += ' <c:majorTickMark val="out"/>';
-			strXml += ' <c:minorTickMark val="none"/>';
+			strXml += ' <c:majorTickMark val="' + (opts.valAxisMajorUnitType ? opts.valAxisMajorUnitType : 'out') + '"/>';
+			strXml += ' <c:minorTickMark val="' + (opts.valAxisMinorUnitType ? opts.valAxisMinorUnitType : 'none') + '"/>';
 			strXml += ' <c:tickLblPos val="'+ (opts.catAxisLabelPos || opts.barDir == 'col' ? 'nextTo' : 'low') +'"/>';
 		}
 		strXml += ' <c:spPr>';
